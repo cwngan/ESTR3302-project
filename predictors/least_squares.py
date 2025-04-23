08483ead8002b8579e7112f246f659befc7655aa
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-04-24 01:47:30 +0800

    Add LeastSquaresPredictor and utility functions for data handling

---
+++ predictors/least_squares.py
@@ -0,0 +1,76 @@
+from typing import override
+import numpy as np
+
+from predictors import Predictor
+from utils import average
+
+
+class LeastSquaresPredictor(Predictor):
+    """
+    A least squares predictor trained with given training data.
+    """
+
+    lmda: float
+    b: np.ndarray
+    average_rating: float
+    prediction: np.ndarray = None
+
+    def __init__(self, training_data: np.ndarray, lmda: float):
+        self.training_data = training_data
+        self.lmda = lmda
+        self.average_rating = average(self.training_data)
+        self._find_bias()
+
+    def _find_bias(self):
+        """
+        Perform baseline prediction with given training set.
+        """
+        a = []
+        c = []
+
+        for i, row in enumerate(self.training_data):
+            for j, col in enumerate(row):
+                if np.ma.is_masked(col):
+                    continue
+                tmp = [0] * (len(self.training_data) + len(row))
+                tmp[i] = 1
+                tmp[len(self.training_data) + j] = 1
+                a.append(tmp)
+                c.append(col - self.average_rating)
+
+        a = np.array(a)
+        c = np.atleast_2d(c).transpose()
+        a_transpose = a.transpose()
+        self.b = np.linalg.solve(
+            a_transpose @ a + self.lmda * np.identity(a_transpose.shape[0]),
+            a_transpose @ c,
+        )
+
+    @override
+    def predict(self, entries):
+        if self.prediction is not None:
+            return [self.prediction[i][j] for i, j in entries]
+        output = [0.0] * len(entries)
+        for idx, entry in enumerate(entries):
+            i, j = entry
+            output[idx] = (
+                self.average_rating
+                + self.b[i][0]
+                + self.b[len(self.training_data) + j][0]
+            )
+        return np.clip(output, 1, 5)
+
+    @override
+    def predict_all(self):
+        if self.prediction is not None:
+            return self.prediction
+        prediction = np.zeros(shape=self.training_data.shape, dtype=float)
+        for i in range(np.size(self.training_data, 0)):
+            for j in range(np.size(self.training_data, 1)):
+                prediction[i][j] = (
+                    self.average_rating
+                    + self.b[i][0]
+                    + self.b[len(self.training_data) + j][0]
+                )
+        self.prediction = prediction.clip(min=1, max=5)
+        return self.prediction
---
+++ utils/__init__.py
@@ -0,0 +1,64 @@
+from typing import Any
+import numpy as np
+
+
+def mask_unknown_values(training_data: Any):
+    res = np.array(training_data, dtype=float)
+    mask = np.less(training_data, 0)
+    return np.ma.masked_array(res, np.ma.make_mask(mask))
+
+
+def get_test_set_mask(test_set: list[tuple[int]], shape: tuple[int]):
+    """
+    Returns a mask for the test set.
+    """
+    mask = np.zeros(shape=shape)
+    for i, j in test_set:
+        mask[i, j] = 1
+    return np.ma.make_mask(mask)
+
+
+def average(data: np.ndarray):
+    """
+    Find the average of a matrix ignoring negative values.
+    """
+    masked_data = np.ma.masked_less(data, 0)
+    return np.average(masked_data)
+
+
+def remove_test_set(training_data: Any, test_set: list[tuple[int]]):
+    """
+    Returns a copy of the training set with items in test set set to -1.
+    """
+    res = mask_unknown_values(training_data)
+    return np.ma.masked_array(res, get_test_set_mask(test_set, res.shape))
+
+
+def get_test_set_matrix(training_data: Any, test_set: list[tuple[int]]):
+    """
+    Returns a matrix representing the data of the test set.
+    """
+    res = np.array(training_data, dtype=float)
+    return np.ma.masked_array(res, ~get_test_set_mask(test_set, res.shape))
+
+
+def mean_square_error(prediction: np.ndarray, known: np.ndarray):
+    """
+    Calculate the mean square error between prediction and known values.
+    """
+    mse = 0
+    ts = 0
+    for i, row in enumerate(prediction):
+        for j, cell in enumerate(row):
+            if np.ma.is_masked(cell) or np.ma.is_masked(known[i][j]):
+                continue
+            mse += (cell - known[i][j]) ** 2
+            ts += 1
+    return mse / ts
+
+
+def root_mean_square_error(prediction: np.ndarray, known: np.ndarray):
+    """
+    Calculate the root mean square error between prediction and known values.
+    """
+    return mean_square_error(prediction, known) ** 0.5
