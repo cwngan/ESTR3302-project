20876deb5df6799df1112d618e0f3aec80926b0d
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-04-24 15:44:13 +0800

    Refactor cosine coefficient calculation and prediction handling for USER correlation

--- predictors/neighbor_correlations.py
+++ predictors/neighbor_correlations.py
@@ -43,16 +43,14 @@ class NeighborCorrelationsPredictor(Predictor):
             np.ma.masked_less(self.training_data, 0) - self.baseline.predict_all()
         )
         self.correlation = correlation
-        self.cosine_coefficients = (
-            self._find_cosine_coefficients(self.error)
-            if self.correlation == Correlation.ITEM
-            else self._find_cosine_coefficients(self.error.T)
-        )
+        self.cosine_coefficients = self._find_cosine_coefficients(self.error)
 
     def _find_cosine_coefficients(self, data: np.ndarray):
         """
         Calculate the cosine coefficient of every pair of columns in data.
         """
+        if self.correlation == Correlation.USER:
+            data = data.T
         m = len(data[0])
         D = np.empty((m, m))
         for i in range(m):
@@ -75,17 +73,30 @@ class NeighborCorrelationsPredictor(Predictor):
         if self.predictions is not None:
             return self.predictions[entries[0]][entries[1]]
         result = self.baseline.predict(entries)
+        error = self.error
+        if self.correlation == Correlation.USER:
+            error = error.T
         for idx, entry in enumerate(entries):
             neighbors = get_neighbors(
-                training_data=self.training_data,
+                training_data=(
+                    self.training_data
+                    if self.correlation == Correlation.ITEM
+                    else self.training_data.T
+                ),
                 cosine_coefficients=self.cosine_coefficients,
                 one=True,
-                entry=entry,
+                entry=(
+                    entry
+                    if self.correlation == Correlation.ITEM
+                    else tuple(reversed(entry))
+                ),
             )
             u, i = entry
+            if self.correlation == Correlation.USER:
+                u, i = i, u
             d_sum = sum(abs(self.cosine_coefficients[i][j]) for j in neighbors)
             for j in neighbors:
-                result[idx] += self.cosine_coefficients[i][j] / d_sum * self.error[u][j]
+                result[idx] += self.cosine_coefficients[i][j] / d_sum * error[u][j]
         return np.clip(result, min=1, max=5)
 
     @override
@@ -97,17 +108,29 @@ class NeighborCorrelationsPredictor(Predictor):
         if self.predictions is not None:
             return self.predictions
         n, m = self.training_data.shape
+        error = self.error
+        if self.correlation == Correlation.USER:
+            n, m = m, n
+            error = error.T
         L = get_neighbors(
-            training_data=self.training_data,
+            training_data=(
+                self.training_data
+                if self.correlation == Correlation.ITEM
+                else self.training_data.T
+            ),
             cosine_coefficients=self.cosine_coefficients,
         )
-        result = self.baseline.predict_all().copy()
+        result = (
+            self.baseline.predict_all().copy()
+            if self.correlation == Correlation.ITEM
+            else self.baseline.predict_all().T.copy()
+        )
         for u in range(n):
             for i in range(m):
                 d_sum = sum(abs(self.cosine_coefficients[i][j]) for j in L[u][i])
                 for j in L[u][i]:
-                    result[u][i] += (
-                        self.cosine_coefficients[i][j] / d_sum * self.error[u][j]
-                    )
+                    result[u][i] += self.cosine_coefficients[i][j] / d_sum * error[u][j]
         self.predictions = np.clip(result, min=1, max=5)
+        if self.correlation == Correlation.USER:
+            self.predictions = self.predictions.T
         return self.predictions
