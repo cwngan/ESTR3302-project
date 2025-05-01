28c3b0c9d37bbf6259080f4e18f179da3bf4905e
Matthew Ngan <58377002+cwngan@users.noreply.github.com>
2025-05-01 18:59:15 +0800

    Include RMSE calculations for training and test predictions

--- latent_trainer.py
+++ latent_trainer.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 from predictors.latent_factor import LatentFactorPredictor
-from utils import get_test_set_matrix, remove_test_set
+from utils import get_test_set_matrix, remove_test_set, root_mean_square_error_entries
 from utils.dataset import Dataset
 
 full_dataset = Dataset(full=True)
@@ -11,7 +11,7 @@ training_data = remove_test_set(full_dataset.user_ratings, full_dataset.test_set
 test_data = get_test_set_matrix(full_dataset.user_ratings, full_dataset.test_set)
 
 u, i = training_data.shape
-k = 2
+k = 8
 latent = LatentFactorPredictor(
     shape=training_data.shape,
     k=k,
@@ -22,7 +22,24 @@ latent = LatentFactorPredictor(
 print(f"{latent.p=}")
 print(f"{latent.q=}")
 
-latent.train(training_data=training_data, max_iterations=100000, tol=1e-4)
+latent.train(training_data=training_data, max_iterations=10000, tol=1e-4)
 
-with open("models/latent", "wb") as f:
+with open("models/latent_" + str(k), "wb") as f:
     pickle.dump(latent, f)
+
+training_predictions = latent.predict(full_dataset.training_set)
+test_predictions = latent.predict(full_dataset.test_set)
+print(f"{training_predictions = }")
+print(f"{test_predictions = }")
+print(f"{latent.p = }")
+print(f"{latent.q = }")
+
+
+rmse_test = root_mean_square_error_entries(
+    test_predictions, full_dataset.test_set, test_data
+)
+print(f"{rmse_test = }")
+rmse_training = root_mean_square_error_entries(
+    training_predictions, full_dataset.training_set, training_data
+)
+print(f"{rmse_training = }")
